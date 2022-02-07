9679cd75c43a9798850abf0d60cd95fe84227942
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-02-07 17:53:18 +0100

    Esercizio completato

--- main.js
+++ main.js
@@ -2,23 +2,45 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-alert('Clicca sul pulstante start per generare 5 numeri, avrai 30 secondi per memorizzarli')
+
 
 const button = document.getElementById('start');
 
-button.addEventListener("click", function numeri(){
+button.addEventListener("click",
 
-    numbers = [];
+    function numeri() {
 
-    for (let i = 1; i <= 5; i++) {
-        let numeri = randomNumber(1, 50)
-        numbers.push(numeri)
+        numbers = [];
+
+        for (let i = 1; i <= 5; i++) {
+            let numeri = randomNumber(1, 80)
+            numbers.push(numeri)
+        }
+
+        document.getElementById('numbers').innerHTML = (numbers);
+
+        setTimeout(cancellaNumeri, 29900)
+        setTimeout(numeriIndovinati, 30000)
+    }
+    
+);
+
+function cancellaNumeri() {
+    document.getElementById('numbers').classList.add("none");
+}
+
+function numeriIndovinati() {
+    
+    let indovinati = prompt('Scrivi i numeri che hai appena visto seguiti da una virgola')
+
+    if (indovinati == numbers) {
+        alert('hai vinto!')
+    } else {
+        alert('Mi dispiace hai perso, riprova!')
     }
 
-    console.log(numbers)
-    document.getElementById('numbers').innerHTML = (numbers);
+}
 
-});
 
 
 
